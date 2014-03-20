6e4dfb227b4610aee9b7f96d7ccffeb96e63f72b
Aditya Bhargava <bluemangroupie@gmail.com>
2014-03-20 00:02:37 -0500

    some code borrowed from mchakravartys gloss-game repo

--- src/ActionKid/Core.hs
+++ src/ActionKid/Core.hs
@@ -15,6 +15,25 @@ import Data.StateVar
 import Control.Lens
 --------------------------------------------------------------------------------
 
+-- | Code borrowed from https://hackage.haskell.org/package/gloss-game-0.3.0.0/docs/src/Graphics-Gloss-Game.html
+-- type Size = (Float, Float)    -- ^width & height
+-- type Rect = (Point, Size)     -- ^origin & extent, where the origin is at the centre
+-- boundingBox :: Picture -> Rect
+-- boundingBox G.Blank                    = ((0, 0), (0, 0))
+-- boundingBox (G.Polygon _)              = error "Graphics.Gloss.Game.boundingbox: Polygon not implemented yet"
+-- boundingBox (G.Line _)                 = error "Graphics.Gloss.Game.boundingbox: Line not implemented yet"
+-- boundingBox (G.Circle r)               = ((0, 0), (2 * r, 2 * r))
+-- boundingBox (G.ThickCircle t r)        = ((0, 0), (2 * r + t, 2 * r + t))
+-- boundingBox (G.Arc _ _ _)              = error "Graphics.Gloss.Game.boundingbox: Arc not implemented yet"
+-- boundingBox (G.ThickArc _ _ _ _)       = error "Graphics.Gloss.Game.boundingbox: ThickArc not implemented yet"
+-- boundingBox (G.Text _)                 = error "Graphics.Gloss.Game.boundingbox: Text not implemented yet"
+-- boundingBox (G.Bitmap w h _ _)         = ((0, 0), (fromIntegral w, fromIntegral h))
+-- boundingBox (G.Color _ p)              = boundingBox p
+-- boundingBox (G.Translate dx dy p)      = let ((x, y), size) = boundingBox p in ((x + dx, y + dy), size)
+-- boundingBox (G.Rotate _ang _p)         = error "Graphics.Gloss.Game.boundingbox: Rotate not implemented yet"
+-- boundingBox (G.Scale xf yf p)          = let (origin, (w, h)) = boundingBox p in (origin, (w * xf, h * yf))
+-- boundingBox (G.Pictures _ps)           = error "Graphics.Gloss.Game.boundingbox: Pictures not implemented yet"
+
 onEnterFrame :: MovieClip a => (Float -> a -> IO a) -> Float -> a -> IO a
 onEnterFrame stepFunc num state = stepFunc num state
 
