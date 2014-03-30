5f9261bcae8a98ed05b2ddc0f05db1727929b315
Aditya Bhargava <bluemangroupie@gmail.com>
2014-03-29 21:14:50 -0500

    what a mess

--- src/Main.hs
+++ src/Main.hs
@@ -1,10 +1,12 @@
-{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE TemplateHaskell, NoMonomorphismRestriction #-}
 import ActionKid
 import Data.Monoid ((<>), mconcat)
 import ActionKid.Utils
 import Control.Lens
 import Graphics.Gloss
 import qualified Debug.Trace as D
+import Data.Maybe
+import Control.Applicative
 
 data Jumping = NotJumping | Jumping Int | Falling Int deriving Eq
 
@@ -40,18 +42,50 @@ makeLenses ''GameState
 
 instance MovieClip GameState where
     attrs = gameAttrs
-    render gs = (displayAll $ [gs ^. player] ++ [gs ^. enemy]) <>
-                  (displayAll (gs ^. gameMap))
+    render gs = (displayAll (gs ^. gameMap)) <> (displayAll $ [gs ^. player] ++ [gs ^. enemy])
+                  
 
-tileMap = concat $ mapWithIndex f t
-    where t = [[1, 1, 1, 1], [1, 0, 0, 1], [1, 0, 0, 1], [1, 1, 1, 1]]
-          f (row, j) = mapWithIndex (func j) row
+tileMap :: [[Int]]
+tileMap =
+    [[1, 1, 1, 1],
+     [1, 0, 0, 0],
+     [1, 0, 0, 0],
+     [1, 1, 0, 0],
+     [0, 0, 0, 1]]
+
+toCoords gs = (x_ // 100, y_ // 100)
+    where x_ = floor $ (-1 * (gs ^. x)) + (gs ^. player.x)
+          y_ = floor $ 400 - gs ^. player.y
+
+get :: Int -> [a] -> Maybe a
+get index arr = if index > length arr - 1 || index < 0
+                   then Nothing
+                   else Just $ arr !! index
+
+above :: GameState -> Int
+above gs = fromMaybe 0 $ (get (y_ - 1) tileMap) >>= get x_
+    where (x_, y_) = toCoords gs
+
+below :: GameState -> Int
+below gs = fromMaybe 0 $ (get (y_ + 1) tileMap) >>= get x_
+    where (x_, y_) = toCoords gs
+
+left :: GameState -> Int
+left gs = fromMaybe 0 $ (get y_ tileMap) >>= get (x_ - 1)
+    where (x_, y_) = toCoords gs
+
+right :: GameState -> Int
+right gs = fromMaybe 0 $ (get y_ tileMap) >>= get (x_ + 1)
+    where (x_, y_) = toCoords gs
+
+renderedTileMap = concat $ mapWithIndex f tileMap
+    where f (row, j) = mapWithIndex (func j) row
           func j (t,i) = y .~ (fromIntegral $ 400 - (j * 100)) $ x .~ (fromIntegral $ i * 100) $ tile
             where tile = if t == 1
                           then SimpleTile def black
                           else SimpleTile def white
 
-gameState = GameState p (x .~ 50 $ es) 10.0 tileMap def
+gameState = GameState p (x .~ 50 $ es) 10.0 renderedTileMap def
   where p  = Tile def blue NotJumping 0.0
         es = Tile def green NotJumping 5.0
 
@@ -69,15 +103,23 @@ checkHit gs = if (gs ^. player) `hits` (gs ^. enemy)
                 then player.tileColor .~ red  $ gs
                 else player.tileColor .~ blue $ gs
 
-move gs = player.x +~ (gs ^. player.moveX) $ gs
-jump gs = case gs ^. player.jumping of
-            NotJumping -> gs
-            Jumping 0  -> player.jumping .~ (Falling 10) $ gs
-            Jumping i  -> player.jumping .~ (Jumping (i-1)) $
-                          player.y +~ 8 $ gs
-            Falling 0  -> player.jumping .~ NotJumping $ gs
-            Falling i  -> player.jumping .~ (Falling (i-1)) $
-                          player.y -~ 8 $ gs
+isJumping (Jumping _) = True
+isJumping _ = False
+
+move gs
+  | gs ^. player.moveX < 0.0 && (left gs == 1) = gs
+  | gs ^. player.moveX > 0.0 && (right gs == 1) = gs
+  | otherwise = player.x +~ (gs ^. player.moveX) $ gs
+jump gs
+  | above gs == 1 && (isJumping (gs ^. player.jumping)) = player.jumping .~ NotJumping $ gs
+  | otherwise = case gs ^. player.jumping of
+                  NotJumping -> gs
+                  Jumping 0  -> player.jumping .~ (Falling 10) $ gs
+                  Jumping i  -> player.jumping .~ (Jumping (i-1)) $
+                                player.y +~ 8 $ gs
+                  Falling 0  -> player.jumping .~ NotJumping $ gs
+                  Falling i  -> player.jumping .~ (Falling (i-1)) $
+                                player.y -~ 8 $ gs
 
 on (EventKey (SpecialKey KeyLeft) Down _ _) gs = return $ player.moveX .~ -10.0 $ gs
 on (EventKey (SpecialKey KeyRight) Down _ _) gs = return $ player.moveX .~ 10.0 $ gs
