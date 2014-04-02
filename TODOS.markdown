3702f5d1bf27f0bfadd9ae49a03d1ff504d24814
Aditya Bhargava <bluemangroupie@gmail.com>
2014-04-01 22:03:48 -0700

    chips challengegs

--- TODOS.markdown
+++ TODOS.markdown
@@ -6,3 +6,4 @@
     http://hackage.haskell.org/package/gloss-juicy
 
 - this guy had the same idea I did: https://github.com/mchakravarty/gloss-game
+- add support for tilemaps
--- src/Main.hs
+++ src/Main.hs
@@ -5,67 +5,33 @@ import ActionKid.Utils
 import Control.Lens
 import Graphics.Gloss
 import qualified Debug.Trace as D
+import Graphics.Gloss.Juicy
+import System.IO.Unsafe
+import Data.Maybe
 
-data Jumping = NotJumping | Jumping Int | Falling Int deriving Eq
-
-data Tile = Tile {
-              _tileColor :: Color,
-              _tileAttrs :: Attributes,
-              _jumping :: Jumping,
-              _moveX :: Float
-}
-
+data Tile = Empty { _ea :: Attributes }
+empty = Empty defaultAttrs
 makeLenses ''Tile
+emptyPNG = fromJust . unsafePerformIO $ loadJuicyPNG "images/empty.png"
 
 instance MovieClip Tile where
-  getAttrs = _tileAttrs
-  setAttrs mc a = mc { _tileAttrs = a }
-  render tile = (color (tile ^. tileColor) $ box 100 100)
+    getAttrs (Empty x) = x
+    setAttrs (Empty _) new = Empty new
+    render (Empty _) = emptyPNG
 
 data GameState = GameState {
-                  _player :: Tile,
-                  _enemy :: Tile,
-                  _gameAttrs :: Attributes
+                    _tiles :: [Tile],
+                    _ga :: Attributes
 }
 
 instance MovieClip GameState where
-    getAttrs = _gameAttrs
-    setAttrs mc a = mc { _gameAttrs = a }
-    render gs = (ActionKid.display . _player $ gs) <> (ActionKid.display . _enemy $ gs)
+    getAttrs = _ga
+    setAttrs gs new = gs { _ga = new }
+    render ga = pictures $ map ActionKid.display (_tiles ga)
 
-makeLenses ''GameState
+gameState = GameState [empty] defaultAttrs
 
-gameState = GameState p (x .~ 50 $ e) defaultAttrs
-  where p = Tile blue defaultAttrs NotJumping 0.0
-        e = Tile green defaultAttrs NotJumping 0.0
+main = run "chips challenge" (500, 500) gameState on stepGame
 
-main = run "test game" (500, 500) gameState on stepGame
-
-stepGame _ gs = return . checkHit . move . jump $ gs
-
-checkHit gs = if (gs ^. player) `hits` (gs ^. enemy)
-                then player.tileColor .~ red  $ gs
-                else player.tileColor .~ blue $ gs
-
-move gs = player.x +~ (gs ^. player.moveX) $ gs
-jump gs = case gs ^. player.jumping of
-            NotJumping -> gs
-            Jumping 0  -> player.jumping .~ (Falling 10) $ gs
-            Jumping i  -> player.jumping .~ (Jumping (i-1)) $
-                          player.y +~ 8 $ gs
-            Falling 0  -> player.jumping .~ NotJumping $ gs
-            Falling i  -> player.jumping .~ (Falling (i-1)) $
-                          player.y -~ 8 $ gs
-
-on (EventKey (SpecialKey KeyLeft) Down _ _) gs = return $ player.moveX .~ -10.0 $ gs
-on (EventKey (SpecialKey KeyRight) Down _ _) gs = return $ player.moveX .~ 10.0 $ gs
-on (EventKey (SpecialKey KeyLeft) Up _ _) gs = return $ if gs ^. player.moveX == -10.0
-                                                          then player.moveX .~ 0.0 $ gs
-                                                          else gs
-on (EventKey (SpecialKey KeyRight) Up _ _) gs = return $ if gs ^. player.moveX == 10.0
-                                                           then player.moveX .~ 0.0 $ gs
-                                                           else gs
-on (EventKey (SpecialKey KeyUp) Down _ _) gs = return $ if gs ^. player.jumping == NotJumping
-                                                          then player.jumping .~ (Jumping 10) $ gs
-                                                          else gs
-on _ state = return state
+on _ gs = return gs
+stepGame _ gs = return gs
