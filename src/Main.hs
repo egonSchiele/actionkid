804dda0e1e19f590e92e2758d09efa756687ccbb
Aditya Bhargava <bluemangroupie@gmail.com>
2014-05-01 17:36:47 -0700

    trying to load image maps

--- src/Main.hs
+++ src/Main.hs
@@ -9,6 +9,10 @@ import qualified Debug.Trace as D
 import Graphics.Gloss.Juicy
 import System.IO.Unsafe
 import Data.Maybe
+import Codec.Picture hiding (readImage)
+import Data.Either
+import Codec.Picture.Repa
+import qualified Data.Vector.Unboxed as U
 
 data Tile = Empty { _ea :: Attributes }
           | Wall  { _wa :: Attributes }
@@ -21,10 +25,27 @@ chip  = Chip def
 makeLenses ''Tile
 deriveMC ''Tile
 
+fromRight (Right x) = x
+
+image :: String -> Picture
 image src = translate x y pic
     where pic@(Bitmap w h _ _) = fromJust . unsafePerformIO . loadJuicy $ src
+    -- where image = fromRight . unsafePerformIO . readImage $ src
+    --       pic@(Bitmap w h _ _) = fromJust . fromDynamicImage . imgToImage $ image
           x = fromIntegral w / 2
           y = fromIntegral h / 2
+
+loadTileMap :: String -> Int -> Int -> [Picture]
+loadTileMap src w h = 
+    where image = (fromRight . unsafePerformIO . readImage $ src) :: Img RGBA
+          vec   = toUnboxed image
+
+-- > :t vec
+-- vec :: U.Vector GHC.Word.Word8
+--
+-- vec U.! 14 => gives you a number (word8)
+-- U.length vec == 4096 (4 channels, RGBA, so really 1024...and it's 
+-- a 32x32 image. 32x32 = 1024).
 
 {-# NOINLINE image #-}
 
