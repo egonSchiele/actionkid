26db8d82e97324c1bf075e3e0c94a73d5dfcbda5
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2015-08-26 12:28:24 +0100

    Refactor - Remove redundancy

--- src/ActionKid/Globals.hs
+++ src/ActionKid/Globals.hs
@@ -1,18 +1,22 @@
 module ActionKid.Globals where
-import Data.IORef
-import System.IO.Unsafe
-import qualified Data.Map as M
-import qualified Graphics.Gloss as G
-import Control.Concurrent
+import           Control.Concurrent
+import           Data.IORef
+import qualified Data.Map           as M
+import qualified Graphics.Gloss     as G
+import           System.IO.Unsafe
+
+-- | Set the IORef to the actual int
+setRef :: a -> IORef a
+setRef = unsafePerformIO . newIORef
 
 -- | Global variable to set board width
 boardWidth :: IORef Int
-boardWidth  = unsafePerformIO $ newIORef 0
+boardWidth  = setRef 0
 
 -- | Global variable to set board height
 boardHeight :: IORef Int
-boardHeight = unsafePerformIO $ newIORef 0
+boardHeight = setRef 0
 
 -- | Global variable to cache game images
 imageCache :: IORef (M.Map String G.Picture)
-imageCache = unsafePerformIO $ newIORef M.empty
+imageCache = setRef M.empty
